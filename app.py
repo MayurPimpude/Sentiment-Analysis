1941961b28e3373fd68d0036955759a06ff36d31
mayur <mayurpimpude@gmail.com>
2022-09-20 22:11:46 +0530

    commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import warnings
 warnings.filterwarnings(action='ignore')
 import numpy as np
 import tensorflow as tf
-#from keras.preprocessing.sequence import pad_sequences
 from keras.utils.data_utils import pad_sequences
 from keras.models import Sequential
 from keras.preprocessing.text import one_hot
@@ -11,31 +10,32 @@ from keras.models import load_model
 
 app = Flask(__name__)
 
+sent_length=20
+voc_size=48040
+model = load_model('data/my_model.h5')
+model.summary()
+
 @app.route('/')
 def web():
     return render_template('home.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST','GET'])
 def home():
-    sent_length=20
-    voc_size=48040
-    #model = pickle.load(open('data/my_model.h5', 'rb'))
-    model = load_model('data/my_model.h5','rb')
-    try:
+    
         if request.method == "POST":
             message = request.form.get("message")
-
+            
             s=[one_hot(words,voc_size) for words in message]
+            print(s)
+
             padded = pad_sequences(s,padding='pre',maxlen=sent_length)
+            print(padded)
 
-            sentiment = model.predict(padded)
-            print('Positive Rate :',sentiment)
-
-        return print("you are successfuly logged in"+message)
-    except:
-        print("error")
-    return render_template('home.html')
+            sentiment = model.predict([[padded]])
+            print(sentiment[0])
+            
+            return render_template('home.html',message=sentiment[0],messag=1-sentiment[0])
  
 
 if __name__ == '__main__':
